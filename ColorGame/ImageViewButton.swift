2a755f76c0a3714e60fa04a94053f45159e7dfc3
Abby Dominguez <abbytorade@icloud.com>
2022-11-10 20:33:36 +0100

    Layout

---
+++ ColorGame/ImageViewButton.swift
@@ -0,0 +1,36 @@
+//
+//  ImageViewButton.swift
+//  ColorGame
+//
+//  Created by Abby Dominguez on 10/11/22.
+//
+
+import UIKit
+
+class ImageViewButton: UIImageView{
+    
+    func resizeImage(targetSize: CGSize){
+        let size = self.image!.size
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(origin: .zero, size: newSize)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        self.image!.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.image! = newImage!
+    }
+}
--- ColorGame/ViewController.swift
+++ ColorGame/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var screenSize = CGRect()
+    private var screenWidth = CGFloat()
+    private var screenHeight = CGFloat()
+    private var margin = 22.0
+    private var imageSizeDivider = CGFloat(2)
+    private var monitorDivider = CGFloat(1.2)
+    
+    @IBOutlet weak var monitorImage: ImageViewMonitor!
+    @IBOutlet weak var buttonGreen: ImageViewButton!
+    @IBOutlet weak var buttonBlue: ImageViewButton!
+    @IBOutlet weak var buttonRed: ImageViewButton!
+    @IBOutlet weak var buttonYellow: ImageViewButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            imageSizeDivider = 3
+            monitorDivider = 1.5
+            margin = 40
+        }
+        screenSize = UIScreen.main.bounds
+        screenWidth = screenSize.width
+        screenHeight = screenSize.height
+        
+        monitorImage.resizeImage(targetSize: CGSize(width: screenWidth/monitorDivider, height: screenWidth/monitorDivider))
+        monitorImage.layer.cornerRadius = 40
+        buttonGreen.resizeImage(targetSize: CGSize(width: screenWidth/imageSizeDivider - margin, height: screenWidth/2 - margin))
+        buttonGreen.layer.cornerRadius = 22
+        buttonYellow.resizeImage(targetSize: CGSize(width: screenWidth/imageSizeDivider - margin, height: screenWidth/imageSizeDivider - margin))
+        buttonYellow.layer.cornerRadius = 22
+        buttonRed.resizeImage(targetSize: CGSize(width: screenWidth/imageSizeDivider - margin, height: screenWidth/imageSizeDivider - margin))
+        buttonRed.layer.cornerRadius = 22
+        buttonBlue.resizeImage(targetSize: CGSize(width: screenWidth/imageSizeDivider - margin, height: screenWidth/imageSizeDivider - margin))
+        buttonBlue.layer.cornerRadius = 22
+        
+    
     }
-
-
 }
-
